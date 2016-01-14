99de6e244020bbc8a94716f773811cbbeb231acb
Paulo Silva <engelik@gmail.com>
2016-01-14 16:10:23 +0000

    added hability to slide with keyboard

--- source/js/9-base.js
+++ source/js/9-base.js
@@ -1,14 +1,3 @@
-var moveSlide = function(slides, current, n){
-  $(slides[current]).hide();
-  current += n;
-  if(current >= slides.length){
-    current = 0;
-  } else if (current < 0){
-    current = slides.length-1;
-  }
-  $(slides[current]).fadeIn();
-  return current;
-};
 var main = function() {
   var currentSlide = 0;
   var slides = $('.slide');
--- source/js/base.js
+++
@@ -1,6 +0,0 @@
-var main = function() {
-	console.log('hello world');
-};
-// end of main function
-
-$(document).ready(main);
